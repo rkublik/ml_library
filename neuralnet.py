4e47664342731743255a87e78361b9594c87032d
rkublik <rkublik@ualberta.net>
2017-09-08 11:45:15 -0400

    classification network passes basic tests.

--- neuralnet.py
+++ neuralnet.py
@@ -6,6 +6,7 @@ Created on Thu Aug 31 09:50:11 2017
 """
 
 import numpy as np
+import scipy.optimize as optimize
 
 class neural_net:
     """ Implements a neural network, following Andrew Ng ML course
@@ -24,52 +25,114 @@ class neural_net:
                 self.weights = w
             else:
                 self.weights = (self.weights, w)
-                
+        self.weights = list(self.weights)
+        
+        
     def sigmoid(self, z):
         """ computes sigmoid of input z """
         
         return 1.0 / (1.0 + np.exp(-z))
         
+    def sigmoidGradient(self,z):
+        h = self.sigmoid(z)
+        return np.multiply(h, 1-h)
         
         
-    def cost_function(self, X, y, lmbda):
+    def roll(self, params):
+        weights = [None] * len(self.weights)
+        start = 0
+        for w in range(len(self.weights)):
+            end = start + self.weights[w].size
+            weights[w] = params[start:end].reshape(self.weights[w].shape)
+            start = end
+            
+        return weights
+        
+    def unroll(self, w):
+        weights = w[0].flatten().reshape(w[0].size,1)
+        for i in range(1, len(w)):
+            weights = np.vstack((weights, w[i].reshape(w[i].size,1)))
+        return weights
+
+    
+    def cost_function(self, weights, X, y, lmbda):
         """ Calculate the cost function given X, y, and 
         regularization parameter lmbda
         
         Also compute cost function gradient. Doing this in the same function to 
         re-use computed values.
         """
+        # forward propagation
+        if len(weights) != len(self.weights):
+            weights = self.roll(weights)
+            
         grad = None
+        grads = [None]*len(weights)
+        for g in range(len(grads)):
+            grads[g] = np.zeros(weights[g].shape)
+            
         a = [None]*len(self.layers)
-        z = [None]*len(self.layers)
+        z = [None]*len(weights)
         
         h = X
         m = float(np.shape(X)[0])
         
         yvec = np.zeros((np.shape(X)[0],self.layers[-1]))
+
         for i in range(int(m)):
             yvec[i,y[i]] = 1
             
-        for w in range(len(self.weights)):
-            a = np.hstack((np.ones((np.shape(h)[0],1)),h))
-            h = self.sigmoid(np.dot(a,self.weights[w].T))
+        a[0] = X
+        for w in range(len(weights)):
+            a[w] =  np.hstack((np.ones((np.shape(a[w])[0],1)),a[w]))
+            z[w] = np.dot(a[w],weights[w].T)
+            a[w+1] = self.sigmoid(z[w])
             
+        h = a[-1]
         # compute cost function
         J = 0
         for k in range(self.layers[-1]):
             J = J + 1/m * (np.dot(-yvec[:,k].T, np.log(h[:,k])) - 
                             np.dot((1 - yvec[:,k]).T, np.log(1-h[:,k])))
         # add regularization:
-        for w in range(len(self.weights)):
-            J = J + lmbda/(2*m) * np.sum(np.square(self.weights[w][:,1:]).flatten())
+        for w in range(len(weights)):
+            J = J + lmbda/(2*m) * np.sum(np.square(weights[w][:,1:]).flatten())
             
+        
+        # Back propagation:
+        d = [None] * len(weights)
+        for t in range(int(m)):
+            d[-1] = h[t,:] - yvec[t,:]
+            for k in xrange(len(d)-2, -1, -1):
+                d[k]  = np.multiply(np.dot(weights[k+1].T, d[k+1])[1:],
+                 self.sigmoidGradient(z[k][t,:]))
+            for k in xrange(len(d)-1, -1, -1):
+                grads[k] = grads[k] + np.dot(d[k].reshape((d[k].size,1)), a[k][t,:].reshape((1,a[k].shape[1])))
+                
+        for g in range(len(grads)):
+            grads[g] = 1/m * grads[g]
+            grads[g][:, 1:] = grads[g][:,1:] + lmbda/m * weights[g][:,1:]
             
-        # Compute cost function gradient
+        # unroll gradients
+        grad = grads[0].reshape((grads[0].size,1))
+        for i in range(1, len(grads)):
+            grad = np.vstack((grad, grads[i].reshape(grads[i].size,1)))
             
         return J, grad
     
-    def sigmoidGradinet(self,z):
-        h = self.sigmoid(z)
-        return np.multiply(h, 1-h)
+    def train(self, X, y, lmbda = 0.001):
+        init_weights = self.unroll(self.weights)
+        params, feval, rc = optimize.fmin_tnc(lambda W: self.cost_function(W, X, y, lmbda), x0 = init_weights)
+        
+        # store the weights
+        self.weights = self.roll(params)
+            
+    def predict(self, X):
+        """ return the index of the output layer with the largest value """
+        h = X
+        for i in range(len(self.weights)):
+            z = np.hstack((np.ones((np.shape(h)[0],1)),h))
+            h = self.sigmoid(np.dot(z,self.weights[i].T))
+        return np.argmax(h, axis = 1)
     
     
--- test_neuralnet.py
+++ test_neuralnet.py
@@ -6,7 +6,7 @@ Created on Wed Aug  2 11:31:52 2017
 """
 
 import unittest
-from mock import MagicMock
+#from mock import MagicMock
 
 import numpy as np
 import scipy.io as sio
@@ -76,6 +76,15 @@ class SigmoidTestCases(unittest.TestCase):
         for i in range(len(h)):
             self.assertAlmostEqual(h[i], res[i])
 
+    def test_sigmoid_gradient(self):
+        layers = (1,1)
+        nnet = nn(layers)
+        g = nnet.sigmoidGradient(np.array([-1, -0.5, 0, 0.5, 1]))
+        a = np.array([0.196612, 0.235004, 0.250000, 0.235004, 0.196612])
+        for i in range(len(g)):
+            self.assertAlmostEqual(g[i], a[i], places = 6)
+
+
 class CostFunctionTestCases(unittest.TestCase):
     """ Test the cost function, using the data from Ng's ML course """
     
@@ -91,7 +100,7 @@ class CostFunctionTestCases(unittest.TestCase):
         nnet = nn(layers)
         nnet.weights = (weights['Theta1'], weights['Theta2'])
         
-        J,_ = nnet.cost_function(data['X'], data['y'], lmbda = 0)
+        J,_ = nnet.cost_function(nnet.weights, data['X'], data['y'], lmbda = 0)
        
         self.assertAlmostEqual(J, 0.287629, places = 6)
         
@@ -108,18 +117,88 @@ class CostFunctionTestCases(unittest.TestCase):
         nnet = nn(layers)
         nnet.weights = (weights['Theta1'], weights['Theta2'])
         
-        J,_ = nnet.cost_function(data['X'], data['y'], lmbda = 1)
+        J,_ = nnet.cost_function(nnet.weights, data['X'], data['y'], lmbda = 1)
        
         self.assertAlmostEqual(J, 0.383770, places = 6)
          
-    def test_sigmoid_gradient(self):
-        layers = (1,1)
+    def test_roll_unroll(self):
+        layers = (3, 5, 3)
         nnet = nn(layers)
-        g = nnet.sigmoidGradinet(np.array([-1, -0.5, 0, 0.5, 1]))
-        a = np.array([0.196612, 0.235004, 0.250000, 0.235004, 0.196612])
-        for i in range(len(g)):
-            self.assertAlmostEqual(g[i], a[i], places = 6)
+        weights = nnet.weights
+        unrolled = nnet.unroll(nnet.weights)
+        rolled = nnet.roll(unrolled)
+        
+        for r in range(len(rolled)):
+            self.assertEqual(rolled[r].shape, weights[r].shape)
+            for i in range(rolled[r].size):
+                self.assertAlmostEqual(rolled[r].flatten()[i], weights[r].flatten()[i])
+        
+    def test_cost_function_gradients(self):
+        """ Check implementation of cost function gradients, bu comparing to 
+        numerically calculated gradients
+        
+        This is so long, it should have its own test cases....
+        """
+        layers = (3, 5, 3)
+        weights = [None] * 2
+        m = 5
+        nnet = nn(layers)
+        
+        for w in range(len(weights)):
+            weights[w] = debugInitializeWeights(layers[w+1], layers[w])
+        X = debugInitializeWeights(m, layers[0]-1)
+        y = np.array(range(m)).reshape((m,1))
+        y = np.mod(y,layers[-1])  
 
+        nnparams = nnet.unroll(weights)
+
+        J, g = nnet.cost_function(nnparams, X, y, lmbda = 0)
+        
+        numgrad = computeNumericalGradient(lambda theta: nnet.cost_function(theta, X, y, lmbda = 0), nnparams)
+        
+        diff = np.linalg.norm(numgrad - g)/np.linalg.norm(numgrad + g)
+        #print np.linalg.norm(numgrad-g), np.linalg.norm(numgrad + g)
+        #print np.hstack((numgrad, g))
+        self.assertAlmostEqual(diff, 0, places = 9)
+        
+        
+        
+class PredictionTestCases(unittest.TestCase):
+    
+    def test_predict_ML(self):
+        data = sio.loadmat('test_data/ex4data1.mat')
+        data['y'] = data['y']-1 # convert to index starting at 0.
+        layers = (400,25,10)
+        nnet = nn(layers)
+        
+        nnet.train(data['X'], data['y'], lmbda = 0.001)
+        pred = nnet.predict(data['X'])
+        print pred
+        self.assertAlmostEqual(np.mean(pred == data['y'])*100,99.94, places = 2)
+        
+        
+        
+''' utility functions '''
+def debugInitializeWeights(output_size, input_size):
+    w = np.zeros((output_size, 1 + input_size))
+    w = np.sin(xrange(1,w.size+1)).reshape(w.shape)/10.0
+    return w
+
+def computeNumericalGradient(func, theta):
+    numgrad = np.zeros(theta.shape)
+    perturb = np.zeros(theta.shape)
+    e = 1e-4
+    for p in range(theta.size):
+        perturb[p] = e
+        loss1,_ = func(theta - perturb)
+        loss2,_ = func(theta + perturb)
+        
+        
+        numgrad[p] = (loss2 - loss1)/(2*e)
+        perturb[p] = 0
+    return numgrad
+        
+        
 
 if __name__=="__main__":
     unittest.main()
